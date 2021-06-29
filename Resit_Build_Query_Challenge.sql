7769cf9c3624d2fcc8ed49a0be0e2c369f2f021d
DANIEL DIAZ <103547416@student.swin.edu.au>
2021-06-29 10:23:03 +1000

    Task 3 Complete

--- Resit_Build_Query_Challenge.sql
+++ Resit_Build_Query_Challenge.sql
@@ -73,3 +73,39 @@ CREATE TABLE ENROLMENT (
 -- SELECT * FROM ENROLMENT
 
 -- TASK 3:
+INSERT INTO SUBJECT (SubjCode, Description) VALUES 
+    ('ICTPRG418', 'Apply SQL to extract & manipulate data'),
+    ('ICTBSB430', 'Create Basic Databases'),
+    ('ICTDBS205', 'Design a Database');
+
+INSERT INTO TEACHER (StaffID, Surname, GivenName) VALUES
+    (98776655, 'Young', 'Angus'),
+    (87665544, 'Scott', 'Bon'),
+    (76554433, 'Slade', 'Chris');
+
+INSERT INTO STUDENT (StudentID, Surname, GivenName, Gender) VALUES
+    ('s12233445', 'Baird', 'Tim', 'M'),
+    ('s23344556', 'Nguyen', 'Anh', 'M'),
+    ('s34455667', 'Hallinan', 'James', 'M'),
+    ('s103547416', 'Diaz', 'Daniel', 'M');
+
+
+INSERT INTO SUBJECTOFFERING (SubjCode, Year, Semester, Fee, StaffID) VALUES
+    ('ICTPRG418', 2019, 1, 200, 98776655),
+    ('ICTPRG418', 2020, 1, 225, 98776655),
+    ('ICTBSB430', 2020, 1, 200, 87665544),
+    ('ICTBSB430', 2020, 2, 200, 76554433),
+    ('ICTDBS205', 2019, 2, 225, 87665544);
+
+INSERT INTO ENROLMENT (StudentID, SubjCode, Year, Semester, Grade, DateEnrolled) VALUES
+    ('s12233445', 'ICTPRG418', 2019, 1, 'D', '02/25/2019'),
+    ('s23344556', 'ICTPRG418', 2019, 1, 'P', '02/15/2019'),
+    ('s12233445', 'ICTPRG418', 2020, 1, 'C', '01/30/2020'),
+    ('s23344556', 'ICTPRG418', 2020, 1, 'HD', '02/26/2020'),
+    ('s34455667', 'ICTPRG418', 2020, 1, 'P', '01/28/2020'),
+    ('s12233445', 'ICTBSB430', 2020, 1, 'C', '02/08/2020'),
+    ('s23344556', 'ICTBSB430', 2020, 2, NULL, '06/30/2020'),
+    ('s34455667', 'ICTBSB430', 2020, 2, NULL, '07/03/2020'),
+    ('s23344556', 'ICTDBS205', 2019, 2, 'P', '07/01/2019'),
+    ('s34455667', 'ICTDBS205', 2019, 2,	'N', '07/13/2019');
+
